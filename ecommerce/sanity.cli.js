529934e195e0ba3a609e310272859b874ab441ba
ayushsonone07 <90081700+ayushsonone07@users.noreply.github.com>
2024-08-30 17:32:18 +0000

    done

--- ecommerce/sanity.cli.js
+++ ecommerce/sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: NEXT_PUBLIC_SANITY_DATASET
+    dataset: NEXT_PUBLIC_SANITY_DATASE
   }
 })
